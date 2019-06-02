64169ce2a3c62025baebcaacf2bb7084d478d0c0
Chitrathep Chitranuwatkul <36376937+Ic3Sandy@users.noreply.github.com>
2019-06-02 14:11:26 +0700

    Update script-install-pull.sh

--- func/script-install-pull.sh
+++ func/script-install-pull.sh
@@ -1,7 +1,7 @@
 installPackage() {
 
     echo "Install package.json"
-    docker-compose -f api/dc-server.yaml up -d
+    docker-compose -f api/docker-compose-server.yaml up -d
     docker exec -it server sh -c "npm install"
     docker exec -it server sh -c "npm rebuild"
     docker exec -it server sh -c "npm install"
